0aedbdbeb2d08529f4f315d93a1c1e13a13f59d6
jdgwartney <davidg@boundary.com>
2014-10-15 12:24:09 -0700

    Add metric definition

---
+++ src/main/java/com/boundary/plugin/MetricDefinition.java
@@ -0,0 +1,57 @@
+package com.boundary.plugin;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class MetricDefinition {
+    /**
+     * Name of the metric, must be globally unique if creating
+     */
+    @JsonProperty
+    private final String name;
+    /**
+     * Description of the metric (optional if updating)
+     */
+    private final String description;
+    /**
+     * Short name to use when referring to the metric (optional if updating)
+     */
+    @JsonProperty
+    private final String displayName;
+    /**
+     * Terse short name when referring to the metric and space is limited, less than 15 characters preferred. (optional if updating)
+     */
+    @JsonProperty
+    private final String displayNameShort;
+    /**
+     * The units of measurement for the metric, can be percent, number, bytecount, or duration (optional if updating)
+     */
+    @JsonProperty
+    private final String unit;
+    /**
+     * When graphing (or grouping at the 1 second interval) the aggregate function that makes most sense for this metric.
+     * Can be sum, avg, max, or min. (optional if updating)
+     */
+    @JsonProperty
+    private final String defaultAggregate;
+    /** 
+     * Expected polling time of data in milliseconds. Used to improve rendering of graphs for non-one-second polled metrics. (optional if updating)
+     */
+    @JsonProperty
+    private final long defaultResolutionMS;
+    /**
+     * Is this metric disabled (optional if updating)
+     */
+    private boolean isDisabled;
+
+    public MetricDefinition() {
+        this.name = "";
+        this.description = "";
+        this.displayName = "";
+        this.displayNameShort = "";
+        this.unit = "";
+        this.defaultAggregate = "";
+        this.defaultResolutionMS = 1000L;
+        this.isDisabled = false;
+    }
+}
+
