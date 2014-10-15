55e30a2e892cd28a87698253430ec31ddfdbecd6
jdgwartney <davidg@boundary.com>
2014-10-15 12:07:53 -0700

    Stub for Metrics

---
+++ src/main/java/com/boundary/plugin/Metric.java
@@ -0,0 +1,30 @@
+package com.boundary.plugin;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Metric {
+
+    @JsonProperty
+    private final String name;
+
+    @JsonProperty
+    private final String displayName;
+
+    public Metric(@JsonProperty("name") String name, @JsonProperty("displayName") String displayName) {
+      this.name = name;
+      this.displayName = displayName;
+    }
+
+    public Metric(String displayName) {
+        this(displayName, displayName);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
+
