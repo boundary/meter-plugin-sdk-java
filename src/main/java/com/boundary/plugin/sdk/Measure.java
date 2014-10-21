b7847240bdae60541e909efd3dca8ba1e6bbd3b8
jdgwartney <davidg@boundary.com>
2014-10-20 20:44:44 -0700

    checkpoint of Measure class

--- src/main/java/com/boundary/plugin/sdk/Measure.java
+++ src/main/java/com/boundary/plugin/sdk/Measure.java
@@ -1,13 +1,70 @@
 package com.boundary.plugin.sdk;
 
+import java.util.Date;
+
 public class Measure {
 	
-    private final String name;
-    private final String value;
-    private final String source;
-    private final Date timestamp;
-
-    public Measure() {
+    private String name;
+    private String value;
+    private String source;
+    private Date timestamp;
+    
+	public Measure() {
+    	name = "";
+    	value = "";
+    	source = null;
+    	timestamp = null;
     }
+	
+	public String toString() {
+		StringBuffer buf = new StringBuffer();
+		buf.append(name);
+		buf.append(" ");
+		buf.append(value);
+		if (source != null) {
+			buf.append(" ");
+			buf.append(source);
+			if (timestamp != null) {
+				buf.append(" ");
+				buf.append(Long.toString(timestamp.getTime()));
+			}
+		}
+		
+		return buf.toString();
+	}
+
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+
 }
 
