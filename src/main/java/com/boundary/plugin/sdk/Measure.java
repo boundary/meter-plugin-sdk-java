618203114e646758b758dc51cf88d01208b429cb
jdgwartney <davidg@boundary.com>
2014-10-20 18:03:19 -0700

    Refactor naming

---
+++ src/main/java/com/boundary/plugin/sdk/Measure.java
@@ -0,0 +1,13 @@
+package com.boundary.plugin.sdk;
+
+public class Measure {
+	
+    private final String name;
+    private final String value;
+    private final String source;
+    private final Date timestamp;
+
+    public Measure() {
+    }
+}
+
