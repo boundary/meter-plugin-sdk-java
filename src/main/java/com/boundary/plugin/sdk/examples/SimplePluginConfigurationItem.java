f291d73a7afe4f90ba50ebc72c1825736144ae0d
jdgwartney <davidg@boundary.com>
2014-11-24 01:44:50 -0800

    Add needed class for parameter items

---
+++ src/main/java/com/boundary/plugin/sdk/examples/SimplePluginConfigurationItem.java
@@ -0,0 +1,26 @@
+package com.boundary.plugin.sdk.examples;
+
+public class SimplePluginConfigurationItem {
+	private String name;
+	private String source;
+	
+	public SimplePluginConfigurationItem() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+}
+
