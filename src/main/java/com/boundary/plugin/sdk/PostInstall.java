5386aba437d76f59f6b27ee23c0bc6d2964a9690
jdgwartney <davidg@boundary.com>
2014-11-23 19:44:22 -0800

    Add stub for handling post install

---
+++ src/main/java/com/boundary/plugin/sdk/PostInstall.java
@@ -0,0 +1,19 @@
+package com.boundary.plugin.sdk;
+
+/**
+ * Stub class for handling post install configuration
+ *
+ */
+public class PostInstall {
+
+	/**
+	 * Default constructor
+	 */
+	public PostInstall() {
+
+	}
+	
+	public static void main(String[] args) {
+		System.exit(0);
+	}
+}
