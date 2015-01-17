86827120ab51f64de5486e128b18b937d189c541
jdgwartney <davidg@boundary.com>
2015-01-16 17:43:16 -0800

    Add quiet option

--- export-mbeans.sh
+++ export-mbeans.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mvn exec:java -Dexec.mainClass=com.boundary.plugin.sdk.jmx.ExportMBeans -Dexec.args="$*"
+mvn -q exec:java -Dexec.mainClass=com.boundary.plugin.sdk.jmx.ExportMBeans -Dexec.args="$*"
 
