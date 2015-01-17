61508677180b32b07786d113409b194ae7cac9c1
jdgwartney <davidg@boundary.com>
2015-01-17 14:48:15 -0800

    Unused files

--- src/test/java/com/boundary/plugin/sdk/PropertiesTest.java
+++
@@ -1,9 +0,0 @@
-package com.boundary.plugin.sdk;
-
-public class PropertiesTest {
-    public static void main(String[] args) throws Exception {
-
-        // display new properties
-        System.getProperties().list(System.out);
-    }
-}
