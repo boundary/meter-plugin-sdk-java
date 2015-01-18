1a82955c4e721e75f38a2e22deb21b6f4947c619
jdgwartney <davidg@boundary.com>
2015-01-18 11:11:39 -0800

    Refactor to address findbugs and better exception handling

--- src/main/java/com/boundary/plugin/sdk/GetPluginJar.java
+++ src/main/java/com/boundary/plugin/sdk/GetPluginJar.java
@@ -23,40 +23,28 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.stream.XMLEventReader;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.Attribute;
-import javax.xml.stream.events.EndElement;
-import javax.xml.stream.events.StartElement;
-import javax.xml.stream.events.XMLEvent;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 public class GetPluginJar implements PostInstall {
-	
-	private static Logger LOG = LoggerFactory.getLogger(PluginUtil.class);
 
-	private final String POM_PATH="pom.xml";
-	private final String JAR_DESTINATION_PATH="config/plugin.json";
-	private final String VERSION_XPATH_EXPRESSION = "/project/version";
-	private final String BASE_URL_XPATH_EXPRESSION = "/project/properties/boundary-jar-base-url";
-	private final String JAR_BASE_NAME_EXPRESSION = "/project/name";
+	private final static String POM_PATH="pom.xml";
+	private final static String JAR_DESTINATION_PATH="config/plugin.json";
+	private final static String VERSION_XPATH_EXPRESSION = "/project/version";
+	private final static String BASE_URL_XPATH_EXPRESSION = "/project/properties/boundary-jar-base-url";
+	private final static String JAR_BASE_NAME_EXPRESSION = "/project/name";
 	private String version;
 	private String baseUrl;
 	private String jarBaseName;
@@ -65,87 +53,86 @@ public class GetPluginJar implements PostInstall {
 
 	}
 	
-	public void readPOM(String pomFile)
-			throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
-		DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+	public void readPOM(String pomFile) throws ParserConfigurationException,
+			SAXException, XPathExpressionException, IOException {
+		DocumentBuilder parser = DocumentBuilderFactory.newInstance()
+				.newDocumentBuilder();
+		try {
+			FileInputStream input = new FileInputStream(new File(pomFile));
+			try {
+				Document document = parser.parse(input);
 
-		FileInputStream input = new FileInputStream(new File(pomFile));
-		Document document = parser.parse(input);
+				XPath xpath = XPathFactory.newInstance().newXPath();
+				this.version = (String) xpath.evaluate(
+						VERSION_XPATH_EXPRESSION, document,
+						XPathConstants.STRING);
+				this.baseUrl = (String) xpath.evaluate(
+						BASE_URL_XPATH_EXPRESSION, document,
+						XPathConstants.STRING);
+				this.jarBaseName = (String) xpath.evaluate(
+						JAR_BASE_NAME_EXPRESSION, document,
+						XPathConstants.STRING);
+				input.close();
+			} catch (IOException e) {
+				throw e;
+			} finally {
+				input.close();
+			}
+		} catch (FileNotFoundException e) {
+			throw e;
+		}
+		// LOG.debug("baseUrl: {}, version: {}, jarBaseName",this.baseUrl,this.version,this.jarBaseName);
+	}
 
-		XPath xpath = XPathFactory.newInstance().newXPath();
-		this.version  = (String)xpath.evaluate(VERSION_XPATH_EXPRESSION,document,XPathConstants.STRING);
-		this.baseUrl = (String)xpath.evaluate(BASE_URL_XPATH_EXPRESSION,document,XPathConstants.STRING);
-		this.jarBaseName = (String)xpath.evaluate(JAR_BASE_NAME_EXPRESSION,document,XPathConstants.STRING);
-		System.out.printf("jarBaseName: %s\n",this.jarBaseName);
-
-		LOG.debug("baseUrl: {}, version: {}, jarBaseName",this.baseUrl,this.version,this.jarBaseName);
-	}		
-	public void downloadJAR() {
+	public void downloadJAR() throws IOException {
 		OutputStream out = null;
 		try {
 			HttpsURLConnection connection = null;
-			String sUrl = String.format("%s/%s/%s-%s",this.baseUrl,this.version,this.jarBaseName,this.version);
-			System.out.println(sUrl);
+			String sUrl = String.format("%s/%s/%s-%s.jar",this.baseUrl,this.version,this.jarBaseName,this.version);
+			System.err.println(sUrl);
 			
-			URL url = new URL("https://github.com/boundary/boundary-plugin-jvm/releases/download/RE-00.07.00/boundary-plugin-jvm-00.07.00.jar");
+			URL url = new URL(sUrl);
 			connection = (HttpsURLConnection) url.openConnection();
 			connection.addRequestProperty("Accept","application/zip");
+			// Ensure that we follow redirects
 			HttpsURLConnection.setFollowRedirects(true);
 
-			
 			InputStream in = connection.getInputStream();
 			out = new FileOutputStream(new File(JAR_DESTINATION_PATH));
 			byte [] b = new byte[1024];
 			connection.connect();
 			
-//			int contentLength = connection.getContentLength();
-//			System.out.printf("length = %d\n",contentLength);
-//			System.out.printf("content-type = %s\n",connection.getContentType());
-			
 			while(in.read(b) != -1) {
 				out.write(b);
 			}
-			out.close();
 			
+			out.close();
+
+		} catch (FileNotFoundException e) {
+			System.err.printf("%s",e.getMessage());
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.printf("%s",e.getMessage());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			out.close();
+			throw e;
 		}
-		finally {
-			try {
-				out.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
 	}
-	
-
 	
 	public void execute(String[] args) {
 		
 		try {
 			readPOM(POM_PATH);
-			//downloadJAR();
+			downloadJAR();
 		} catch (XPathExpressionException e) {
-			LOG.error("XPathExpressionException: ",e.getMessage());
+			System.err.printf("%s",e.getMessage());
 		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.printf("%s",e.getMessage());
 		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.printf("%s",e.getMessage());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.printf("%s",e.getMessage());
 		}
 	}
-	
 	
 	public static void main(String [] args) {
 		GetPluginJar gpJar = new GetPluginJar();
